bf7d011f80b8b755c368387beae9cf5a1149c20a
Tim MacDonald <hello@timacdonald.me>
2023-05-17 14:22:33 +1000

    wip

--- packages/vue/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { Config } from 'laravel-precognition/dist/types';
-export declare const useForm: <TForm extends Record<string, unknown>>(method: RequestMethods, url: string, data?: TForm | undefined, config?: Config) => PrecognitiveForm;
