3d976027c35a16641042c1292f992b859a2587e6
Quido Hoekman <quido.hoekman@dij.digital>
2024-01-22 12:39:40 +0100

    fix(sec): updated lodash to 4.17 due to CWE-1321 in the old package lodash.set

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,7 +1,7 @@
 import { resolveName, client, createValidator, Config, RequestMethod, Validator, toSimpleValidationErrors, ValidationConfig, resolveUrl, resolveMethod } from 'laravel-precognition'
-import cloneDeep from 'lodash.clonedeep'
-import get from 'lodash.get'
-import set from 'lodash.set'
+import cloneDeep from 'lodash/cloneDeep'
+import get from 'lodash/get'
+import set from 'lodash/set'
 import { useRef, useState } from 'react'
 import { Form } from './types.js'
 
