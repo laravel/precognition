52cc35dc650f4928a6033101bdac5029a3f545fe
Tim MacDonald <hello@timacdonald.me>
2024-05-23 11:17:31 +1000

    wip

--- packages/core/src/error.ts
+++ packages/core/src/error.ts
@@ -1,6 +1,6 @@
 export class PrecognitionError extends Error {
   constructor(message: string, options?: { cause: Error }) {
-      // @ts-ignore
+    // @ts-expect-error
     super(message, options)
   }
 }
