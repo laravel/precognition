79f35c3d9fba9b1bfba01598efcc470d7bf367c8
Joel Stein <joel@mediatrix.digital>
2025-02-21 10:17:49 -0600

    Exports the Form type as PrecognitionForm.

--- packages/react-inertia/src/index.ts
+++ packages/react-inertia/src/index.ts
@@ -6,6 +6,7 @@ import { useRef } from 'react'
 import { Form, FormDataConvertible } from './types'
 
 export { client }
+export { Form as PrecognitionForm }
 
 export const useForm = <Data extends Record<string, FormDataConvertible>>(method: RequestMethod | (() => RequestMethod), url: string | (() => string), inputs: Data, config: ValidationConfig = {}): Form<Data> => {
     const booted = useRef<boolean>(false)
